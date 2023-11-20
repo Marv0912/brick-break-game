3f3e900f335f8ac3cde589f461c9a7838a22b501
Marv0912 <marvin.fajardo.s@icloud.com>
2023-11-20 12:20:08 -0400

    add main html and css file and structure

--- js/paddle.js
+++ js/paddle.js
@@ -2,8 +2,17 @@ class Paddle {
     constructor() {
         this.width = 50;
         this.height = 20;
+        this.direction = 250;
         this.speed = 0;
         this.maxSpeed = 10;
+    }
+    move() {
+        if (this.direction <= 0) {
+            this.direction *= -1;
+        }
+        else if (this.direction >= 500) {
+            this.direction *= -1
+        }
     }
 }
 
