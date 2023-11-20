4b0a10679d95decd0fdc6f99abfbc7db7c2180b1
Marv0912 <marvin.fajardo.s@icloud.com>
2023-11-20 16:32:32 -0400

    create constructor method for Ball class

--- js/ball.js
+++ js/ball.js
@@ -1,12 +1,10 @@
 class Ball{
-    constructor() {
-    this. =    
-    this. =    
-    this. =    
-    this. =    
-    this. =    
-    this. =    
-    this. =    
-    this. =  
+    constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed = 0;
+    this.directionX = 0; 
+    this.directionY = 0;  
     }
 }
