2de4e84de0400208d692782f9d1b8457e6cb2f1e
Marv0912 <marvin.fajardo.s@icloud.com>
2023-11-28 17:01:06 -0400

    create brick class constructor method

--- js/brick.js
+++ js/brick.js
@@ -1,13 +1,18 @@
-class Brick{
-    constructor() {
-        this. =    
-        this. =    
-        this. =    
-        this. =    
-        this. =    
-        this. =    
-        this. =    
-        this. =    
+class Brick {
+    constructor(gameScreen, x, y, width, height, element) {
+        this.gameScreen = gameScreen;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.status = 1;
+        this.element = document.createElement('img');
+        this.element.src = `${element}`;
+        this.element.style.position = 'absolute';
+        this.element.style.width = `${this.width}px`;
+        this.element.style.height = `${this.height}px`;
+        this.element.style.left = `${this.x}px`;
+        this.element.style.top = `${this.y}px`;
+        this.gameScreen.appendChild(this.element);
     }
-
 }
